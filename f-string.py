abc57665aef535e1e4ff75795a4c5efcab98093c
arvind95544 <77373328+arvind95544@users.noreply.github.com>
2021-01-25 19:10:35 +0530

    Add files via upload

---
+++ f-string.py
@@ -0,0 +1,10 @@
+# To save time by using one line functionis called f-string
+lst=["Arvind","Priyank","Avnish","Ashish"]
+length=len(lst)
+for i in range(length):
+   print(lst[i],f"is student of MJP Rohilkhnad University Barielly")
+
+
+a="Python"
+b="Programing"
+print(f"Welcome to {a} {b}")
