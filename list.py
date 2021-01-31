9ee26d1aee45405330e74921ece261b78c176e2d
arvind95544 <77373328+arvind95544@users.noreply.github.com>
2021-01-31 19:23:42 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+"""var1=5
+var2=50
+print("Enter value")
+var3=int(input())
+if var3>var2:
+    print("Greater than 50")
+elif var3==var2:
+    print("Eqal to 50")
+else:
+    print("lasser than 50")"""
+"""list=[3,7,9,5]
+if 9 in list:
+    print("Yes its in list")"""
+var4=18
+print("Enter age")
+var5=int(input())
+if var5>var4:
+    print("You can drive")
+elif var5==var4:
+    print("We can't decide")
+else:
+    print("You can't drive")
