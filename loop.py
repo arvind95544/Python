dd91506ccda6526e4b2e88638d3eb90e5035c841
arvind95544 <77373328+arvind95544@users.noreply.github.com>
2021-01-31 19:23:04 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+"""list = [1,4,6,8,9,13,10,5]
+for item in list:
+    if item>6:
+        print(item)"""
+"""list = [1,4,'C++',6,8,'Java',9,13,'Python',10,'c',5]
+for item in list:
+    if str(item).isnumeric() and item>6:
+        print(item)"""
+x=8
+for x in range(5):
+    print("Hello")
+    print(x)
