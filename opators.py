53125190b21c347c95561429821fe3f8b5aed3b3
arvind95544 <77373328+arvind95544@users.noreply.github.com>
2021-01-31 19:21:14 +0530

    Add files via upload

---
+++ opators.py
@@ -0,0 +1,22 @@
+#Arithmetic operators
+"""print("10+5",10+5)
+print("10-5",10-5)
+print("10*5",10*5)
+print("10/5",10/5)
+print("10%5",10%5)
+print("10//4",10//5)
+print("10**2",10**2)"""
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    c=a+b
+    return c
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+"""def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    num3 = a+b
+    return num3
+res = solveMeFirst(5,15)
+print(res)"""
