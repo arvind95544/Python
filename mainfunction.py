4ca9dd17a6e7452ac736ee25d68729d49b3b737c
arvind95544 <77373328+arvind95544@users.noreply.github.com>
2021-01-31 19:22:29 +0530

    Add files via upload

---
+++ mainfunction.py
@@ -0,0 +1,11 @@
+def example():
+    print("Hello What are you doing.")
+
+def add(a,b):
+    c=a+b
+    return c
+if __name__ == '__main__':
+
+    print("Before calling")
+    var=add(5,6)
+    print("Sum of these two number",var)
